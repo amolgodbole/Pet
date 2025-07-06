bef324b5ee5048b9d75770df179bb0915b7c282f
amolgodbole <amolgodbole@users.noreply.github.com>
2025-07-05 20:12:01 -0700

    Create top_using_ps.sh

---
+++ starter_problems/reverse_array/top_using_ps.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+PID=$(prgrep -d "," bgpd)
+OUTPUT_FILE="top-output.txt"
+DURATION=900
+INTERVAL=0.01
+
+# Clear the output file and add header
+echo "Timestamp,PID,Command,CPU%,Memory%" > "$OUTPUT_FILE"
+
+END_TIME=$(($(date +%s) + DURATION))
+
+while [ $(date +%s) -lt $END_TIME ]; do
+    TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S.%3N')
+    
+    # Use ps to get process information
+    ps -p $PID -o pid,comm,%cpu,%mem --no-headers | while read pid comm cpu mem; do
+        echo "$TIMESTAMP,$pid,$comm,$cpu,$mem" >> "$OUTPUT_FILE"
+    done
+    
+    sleep $INTERVAL
+done
